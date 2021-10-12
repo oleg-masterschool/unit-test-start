b27e9cdeab43e0096f2aa1438e1bc609ff908dae
oleg-masterschool <87778112+oleg-masterschool@users.noreply.github.com>
2021-10-12 22:30:03 +0300

    sanity tests

--- src/Vehicle/vehicle.spec.js
+++ src/Vehicle/vehicle.spec.js
@@ -1,4 +1,11 @@
 // placeholder for Vehicle class tests
-test('sanity', () => {
-
-})
+test('sanity', () => { // test
+    expect(true).not.toBe(false); // assertion
+    expect(1 + 1).toBe(2);
+    expect('Learning tests').toHaveLength(14);
+    expect(false).toBeFalsy();
+    expect(typeof null).toBe('object');
+    // expect({}).toBe({}); // fails
+    expect({}).toEqual({});
+    // expect({x: 1, y: 0}).toEqual({ x: 1});
+});
